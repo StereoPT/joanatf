ef4d92c3f656c92945800b2003e9e36f0877d00e
Guido <stereopt@gmail.com>
2023-01-21 15:24:34 +0000

    feat: added tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h2>Hello World</h2>
+        <h1 className='text-3xl font-bold underline'>Hello World</h1>
       </main>
     </>
   );

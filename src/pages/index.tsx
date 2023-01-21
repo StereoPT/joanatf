ef560e26a7baacf3316186008f4c097576de9769
Guido <stereopt@gmail.com>
2023-01-21 15:13:23 +0000

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Joana Santos - Página Inicial</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Hello World</h2>
+      </main>
+    </>
+  );
+}
